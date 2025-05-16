a997782a91925407df3c4db57d8d70664404860f
KellyHarvestOS <vladjjjsss7@gmail.com>
2025-05-16 14:00:15 +0500

    feat: Implement Add Game page

---
+++ app/(main)/games/add/page.tsx
@@ -0,0 +1,10 @@
+// app/(main)/games/add/page.tsx
+import GameForm from '@/components/GameForm';
+
+export default function AddGamePage() {
+  return (
+    <div>
+      <GameForm />
+    </div>
+  );
+}
