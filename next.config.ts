bbd5af6d70ef614b4367ea2d26e7c69b6811863d
KellyHarvestOS <vladjjjsss7@gmail.com>
2025-05-18 00:28:47 +0500

    Feat: Expand game list and resolve image hostname errors
    Added new games and updated next.config.js with required image domains (wikimedia, steam, playstation, googleusercontent, gstatic).

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
-import type { NextConfig } from "next";
+// next.config.js
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/wikipedia/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.akamai.steamstatic.com',
+        port: '',
+        pathname: '/steam/apps/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.api.playstation.com',
+        port: '',
+        pathname: '/vulcan/ap/rnd/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'play-lh.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      { 
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/images/**', 
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
